315b3d4bab314e9ab90c768b1086a33526a5c2ae
rayen288700 <rayenchuk@gmail.com>
2023-09-25 12:28:19 +0100

    multiline string

--- python-hello_world/9-easter_egg.py
+++ python-hello_world/9-easter_egg.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str=""" The Zen of Python, by Tim Peters
+print(""" The Zen of Python, by Tim Peters
 
 Beautiful is better than ugly.
 Explicit is better than implicit.
@@ -19,4 +19,4 @@ Now is better than never.
 Although never is often better than *right* now.
 If the implementation is hard to explain, it's a bad idea.
 If the implementation is easy to explain, it may be a good idea.
-Namespaces are one honking great idea -- let's do more of those!"""
+Namespaces are one honking great idea -- let's do more of those!""")
