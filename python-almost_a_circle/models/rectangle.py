293cc332c8df2350a1639deffdffef64f1ff52e0
Rayen-JN <rayenchuk@gmail.com>
2023-11-17 09:09:39 +0100

    change the operator

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -19,7 +19,7 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, value):
-        if type(value) != int:
+        if type(value) is not int:
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
