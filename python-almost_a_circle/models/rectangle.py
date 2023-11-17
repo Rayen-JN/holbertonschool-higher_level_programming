5266bf5b5d9554b34d337005383fa43f8b2a216c
Rayen-JN <rayenchuk@gmail.com>
2023-11-17 09:10:20 +0100

    change the operator

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -32,7 +32,7 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
 
-        if type(value) != int:
+        if type(value) is not int:
             raise TypeError("height must be an integer")
         if value <= 0:
             raise ValueError("height must be > 0")
